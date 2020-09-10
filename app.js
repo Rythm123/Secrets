94e42a9913bd2448323b72c9b6b9cf196326eee1
Rythm Gupta <rythmgupta74@gmail.com>
2020-09-10 16:10:52 +0530

    Add username and password along with encryption

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
+
+
+const app=express();
+app.use(express.static("public"))
+
+app.set("view-engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+const userSchema=new mongoose.Schema({
+  name:String,
+  password:String
+});
+
+const secret="Thisisasecretformysecretproject."
+userSchema.plugin(encrypt,{secret:secret , encryptedFields:["password"]});
+
+const User=mongoose.model("User",userSchema);
+
+
+app.get("/",function(req,res){
+  res.render("home.ejs");
+});
+
+app.get("/login",function(req,res){
+  res.render("login.ejs");
+});
+
+app.get("/register",function(req,res){
+  res.render("register.ejs");
+});
+
+app.post("/register",function(req,res){
+  const newUser=new User({
+    name:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(!err){
+      res.render("secrets.ejs");
+    }
+    else{
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+  User.findOne({name:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }
+    else{
+      if(foundUser){
+        if(foundUser.password===password){
+          res.render("secrets.ejs");
+        }
+        else{
+          res.redirect("/login")
+        }
+      }
+    }
+  });
+});
+
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+})
